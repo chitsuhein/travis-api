b611d1d1f903f0e93e39d3c9f5e73e10dd263435
Aakriti Gupta <aakriti@travis-ci.org>
2016-09-20 17:40:20 +0200

    Re-factor

--- lib/travis/api/v3/queries/crons.rb
+++ lib/travis/api/v3/queries/crons.rb
@@ -5,18 +5,6 @@ module Travis::API::V3
       Models::Cron.where(branch_id: repository.branches)
     end
 
-    def enqueue_all
-      scheduled_crons.each do |cron|
-        begin
-          cron.needs_new_build? ? cron.enqueue : cron.skip_and_schedule_next_build
-        rescue => e
-          Raven.capture_exception(e, tags: { 'cron_id' => cron.try(:id) })
-          sleep(10) # This ensures the dyno does not spin down before the http request to send the error to sentry completes
-          next
-        end
-      end
-    end
-
     def scheduled_crons
       Models::Cron.where("next_run <= '#{DateTime.now.utc}'")
     end
--- lib/travis/api/v3/services/crons/start.rb
+++ lib/travis/api/v3/services/crons/start.rb
@@ -4,7 +4,23 @@ module Travis::API::V3
   class Services::Crons::Start < Service
     def run!
       run_periodically(Travis::API::V3::Cron::SCHEDULER_INTERVAL) do
-        query.enqueue_all
+        enqueue_all
+      end
+    end
+
+    def query
+      @query ||= Queries::Crons.new({}, 'Overview')
+    end
+
+    def enqueue_all
+      query.scheduled_crons.each do |cron|
+        begin
+          cron.needs_new_build? ? cron.enqueue : cron.skip_and_schedule_next_build
+        rescue => e
+          Raven.capture_exception(e, tags: { 'cron_id' => cron.try(:id) })
+          sleep(10) # This ensures the dyno does not spin down before the http request to send the error to sentry completes
+          next
+        end
       end
     end
   end
