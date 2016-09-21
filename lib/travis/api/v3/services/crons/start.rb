10eaf8a90dd31bd0bc202a897ecca02980e06a23
Aakriti Gupta <aakriti@travis-ci.org>
2016-09-21 15:39:01 +0200

    repalce run_periodically because it crashes; test

--- lib/travis/api/v3/services/crons/start.rb
+++ lib/travis/api/v3/services/crons/start.rb
@@ -1,19 +1,10 @@
-require "core_ext/kernel/run_periodically"
-
 module Travis::API::V3
   class Services::Crons::Start < Service
     def run!
       Travis.logger.info "DB connection to: #{ActiveRecord::Base.connection.current_database}"
-      Thread.new do
-        Travis.logger.info "In new thread"
-        begin
-          loop do
-            enqueue_all
-            sleep(5.second)#Cron::SCHEDULER_INTERVAL)
-          end
-        rescue Exception => e
-          Travs.logger.info "Enqueuing crashed with #{e.message}"
-        end
+      loop do
+        enqueue_all
+        sleep(Cron::SCHEDULER_INTERVAL)
       end
     end
 
